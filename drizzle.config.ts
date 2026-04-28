4ad59cc8414e00bb948c1ac6668834db25f536fa
ranarasul221 <miahrana485@gmail.com>
2026-04-29 04:46:28 +0600

    added chat app

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
 
-export default defineConfig({
+export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+} satisfies Config;
